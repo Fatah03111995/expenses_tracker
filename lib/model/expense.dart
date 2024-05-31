600b68f76b63a3fb75d5cd66b5ef8005d34d1fc6
Abdul Fatah <fatah03111995@gmail.com>
2024-05-31 13:02:48 +0700

    feat : expense data model

---
+++ lib/model/expense.dart
@@ -0,0 +1,22 @@
+import 'package:uuid/uuid.dart';
+
+class Expense {
+  final String id;
+  String title;
+  double amount;
+  final DateTime createdAt;
+  DateTime? editedAt;
+
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.createdAt,
+  }) : id = const Uuid().v4();
+  // : propertyA = value, propertyB = value (to initialize parameter that is never fill)
+
+  void editExpense({String? title, double? amount}) {
+    this.title = title ?? this.title;
+    this.amount = amount ?? this.amount;
+    editedAt = DateTime.now();
+  }
+}
